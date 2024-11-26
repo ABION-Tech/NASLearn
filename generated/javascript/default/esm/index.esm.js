2a062c3a98f02fe21563ac0dcd1d5f7c4c59de54
NewAgeSkillsphere <newageskillsphere@gmail.com>
2024-11-26 03:17:56 +0100

    Initial commit

---
+++ generated/javascript/default/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'learn',
+  location: 'us-central1'
+};
+
