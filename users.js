f82696d9cd3de558e59b6316cea06b987f55a49e
ABION-Tech <abionindustries@gmail.com>
2025-01-04 16:25:03 +0100

    Newcommit

---
+++ users.js
@@ -0,0 +1,4 @@
+[
+    { "email": "student1@example.com", "password": "5e884898da28047151d0e56f8dc6292773603d0d" }, 
+    { "email": "student2@example.com", "password": "d8578edf8458ce06fbc5bb76a58c5ca4" }
+]
