a5c90973ddda689fdfcb4b1c571b78dd571d9104
ABION-Tech <abionindustries@gmail.com>
2025-07-11 06:37:48 +0100

    new

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'nas-lms-v1';
+const assetsToCache = [
+  '/login.html',
+  '/css/bootstrap.min.css',
+  '/js/main.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      // Optional: fallback page/image if offline
+    })
+  );
+});
