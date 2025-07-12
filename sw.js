0d73add25079f5783924c6b8db84cf70874e71af
ABION-Tech <abionindustries@gmail.com>
2025-07-12 08:05:00 +0100

    login

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'nas-cache-v2';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/css/bootstrap.min.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request)
+            .then(res => res || fetch(e.request))
+    );
+});
